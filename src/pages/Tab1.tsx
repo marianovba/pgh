f16997be6f9bbf8ed926d7272222cde9ade40f72
marianovba <mariano.valenzuela945@gmail.com>
2025-03-13 19:32:45 -0700

    Codigo textbox fill

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,13 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton, IonItem, IonLabel } from '@ionic/react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const [userName, setUserName] = useState<string>('');
+  const [greeting, setGreeting] = useState<string>('world'); // Estado inicial para el saludo
+
+  const updateGreeting = () => {
+    if (userName) {
+      setGreeting(userName); // Actualiza el saludo con el valor introducido
+    } else {
+      setGreeting('world'); // Si no hay valor, vuelve a "world"
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Hello App!</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +27,27 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        {/* Saludo inicial */}
+        <div className="greeting-container">
+          <span style={{ color: 'cornflowerblue' }}>Hello </span>
+          <span style={{ color: 'white' }}>{greeting}!</span>
+        </div>
+
+        {/* Campo de texto para ingresar el nombre */}
+        <IonItem className='spacing'>
+          <IonLabel position="floating">Introduce tu nombre</IonLabel>
+          <IonInput
+            value={userName}
+            onIonChange={(e) => setUserName(e.detail.value!)}
+            placeholder="Your name"
+          />
+        </IonItem>
+
+        {/* Botón para actualizar el saludo */}
+        <IonButton expand="full" onClick={updateGreeting}>
+          Actualizar Saludo
+        </IonButton>
       </IonContent>
     </IonPage>
   );
